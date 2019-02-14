6c83484a2f94ca1919865bf22b0e703f82c5e7db
Eric MacFarland <ericmacfarlandl@northwesternmutual.com>
2019-02-14 05:10:42 +0000

    Enable deploy to application server

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM registry.uwm-nm-te-capstone.com:8083/node-docker:latest
+FROM registry.uwm-nm-te-capstone.com:8083/general/node-docker:latest
 
 COPY . /app
 
 # Create app directory
 WORKDIR /app
 
-RUN npm install && npm run build
+RUN npm install \
+    && npm rebuild node-sass \
+    && npm run build
 
 CMD ["npm", "start"]
--- deploy/dockerImageUpdateAndStart.sh
+++ deploy/dockerImageUpdateAndStart.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 
-# any future command that fails will exit the script
+###############################################################################
+# Project-specific variables, these should be updated per-project             
+CONTAINER_PORT=4000
+HOST_PORT=4000
+###############################################################################
+
+
+# Any future command that fails will exit the script
 set -e
 
-CI_PROJECT_NAME=$1
-DOCKER_REPO_USER=$2
-DOCKER_REPO_PASS=$3
+# Script arguments
+CI_PROJECT_PATH=$1
+CI_PROJECT_NAME=$2
+CI_COMMIT_SHA=$3
+DOCKER_REPO_USER=$4
+DOCKER_REPO_PASS=$5
 
-# Delete previously existing docker image and container
-echo "remove previously existing container and image for $CI_PROJECT_NAME"
+# Helper variables
+DOCKER_REGISTRY="registry.uwm-nm-te-capstone.com:8083"
+IMAGE_NAME="$DOCKER_REGISTRY/$CI_PROJECT_PATH"
+IMAGE_TAG=$CI_COMMIT_SHA
+FULL_IMAGE_REF="$IMAGE_NAME:$IMAGE_TAG"
+CONTAINER_NAME=$CI_PROJECT_NAME
 
-if [ "$(docker ps -aq -f status=running -f name=$CI_PROJECT_NAME)" ]; then
-echo "docker kill $CI_PROJECT_NAME"
-docker kill $CI_PROJECT_NAME
-echo "docker rm $CI_PROJECT_NAME"
-docker rm  $CI_PROJECT_NAME
+# Delete previously existing docker image(s) and container
+echo "Removing previously existing container $CONTAINER_NAME ..."
+if [ "$(docker ps -aq -f status=running -f name=$CONTAINER_NAME)" ]; then
+    docker kill $CONTAINER_NAME
+    docker rm  $CONTAINER_NAME
 fi
 
-if [[ ! "$(docker images -q registry.uwm-nm-te-capstone.com:8083/$CI_PROJECT_NAME:latest 2> /dev/null)" == "" ]]; then
- echo "docker rmi registry.uwm-nm-te-capstone.com:8083/$CI_PROJECT_NAME:latest"
- docker rmi registry.uwm-nm-te-capstone.com:8083/$CI_PROJECT_NAME:latest
+echo "Removing existing docker images for $IMAGE_NAME ..."
+if [[ ! "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]]; then
+    docker rmi -f $(docker images -q $IMAGE_NAME)
 fi
 
-# pull the latest docker image
-echo "docker pull registry.uwm-nm-te-capstone.com:8083/$CI_PROJECT_NAME:latest"
-docker login registry.uwm-nm-te-capstone.com:8083 --username $DOCKER_REPO_USER --password $DOCKER_REPO_PASS
-docker pull registry.uwm-nm-te-capstone.com:8083/$CI_PROJECT_NAME:latest
+# Pull the requested docker image
+echo "Pulling $FULL_IMAGE_REF"
+docker login $DOCKER_REGISTRY --username $DOCKER_REPO_USER --password $DOCKER_REPO_PASS
+docker pull $IMAGE_NAME:$IMAGE_TAG
 
-#run docker image
-echo "docker run -d -p 4000:4000 --name $CI_PROJECT_NAME registry.uwm-nm-te-capstone.com:8083/$CI_PROJECT_NAME:latest"
-docker run -d -p 4000:4000 --name $CI_PROJECT_NAME registry.uwm-nm-te-capstone.com:8083/$CI_PROJECT_NAME:latest
+# Run the docker image
+echo "Running '$CONTAINER_NAME' on port $HOST_PORT from '$FULL_IMAGE_REF'"
+docker run -d \
+    -p $HOST_PORT:$CONTAINER_PORT \
+    --name $CONTAINER_NAME \
+    -e NODE_ENV=deployed \
+    $FULL_IMAGE_REF
