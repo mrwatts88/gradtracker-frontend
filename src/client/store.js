91813568be098c8bd77c9e269cd1182d977e102b
HAV2461 <aliaksandrahavia@northwesternmutual.com>
2019-09-05 10:45:57 -0400

    Added tests to Concatenation

--- src/client/store.js
+++ src/client/store.js
@@ -6,8 +6,8 @@ import thunkMiddleware from 'redux-thunk';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunkMiddleware))
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunkMiddleware))
 );
 
 export default store;
