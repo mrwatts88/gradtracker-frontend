65377295c4e628f46e7d3693687e3093b7ec5ace
xorai <smit2586@uwm.edu>
2019-10-12 17:08:53 -0500

    clean up code

--- src/server/routes/formDefRoutes.js
+++ src/server/routes/formDefRoutes.js
@@ -6,37 +6,23 @@ const config = require('config');
 const serviceUrl = config.get('serviceUrl');
 
 router.get('/:id', (req, res, next) => {
-  console.log('SELECTING SPECIFIC FORM');
   return axios
     .get(`${serviceUrl}/formDef/${req.params.id}`, req.body)
-    .then(response => {
-      return res.send(response.data);
-    })
-    .catch(err => {
-      next(err);
-    });
+    .then(response => res.send(response.data))
+    .catch(err => next(err));
 });
 
 router.get('/', (req, res, next) => {
-  console.log('in get all forms');
   return axios
     .get(`${serviceUrl}/formDef/`, req.body)
-    .then(response => {
-      return res.send(response.data);
-    })
-    .catch(err => {
-      next(err);
-    });
+    .then(response => res.send(response.data))
+    .catch(err => next(err));
 });
 
 router.post('/', (req, res, next) => {
-  console.log('in formdef post');
   return axios
     .post(`${serviceUrl}/formDef/`, req.body)
-    .then(response => {
-      console.log('in response');
-      return res.send(response.data);
-    })
+    .then(response => res.send(response.data))
     .catch(err => next(err));
 });
 
