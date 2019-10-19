dd33dc54304333733922e0503e3597db5da55790
xorai <smit2586@uwm.edu>
2019-10-18 20:32:29 -0500

    added register user functionality

--- src/client/services/AuthService/authService.js
+++ src/client/services/AuthService/authService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import { authHeader } from '../../helpers/authHeader';
 
 export const AUTH_URL = `${CONTEXT_ROOT}/api/`;
 
 export const authService = {
-    authenticate: (email, password) => axios.post(`${AUTH_URL}auth`, { email, password })
+    authenticate: (email, password) => axios.post(`${AUTH_URL}auth`, { email, password }),
+    register: (newUser) => axios.post(`${AUTH_URL}user`, newUser, { headers: authHeader() })
 };
