da84d6c8374ffea127e46864c2c3c5e514a3c277
Matt Watts <matthewwatts@northwesternmutual.com>
2019-10-22 15:37:31 -0500

    Add authService#register test

--- src/client/services/AuthService/authService.test.js
+++ src/client/services/AuthService/authService.test.js
@@ -3,32 +3,55 @@ import axios from 'axios';
 import { authService, AUTH_URL } from './authService';
 
 describe('authService', () => {
-    const mock = new MockAdapter(axios);
+  const mock = new MockAdapter(axios);
 
-    beforeEach(() => {
-        mock.reset();
+  beforeEach(() => {
+    mock.reset();
+  });
+
+  describe('authenticate', () => {
+    it('makes a post to the AUTH_URL/auth url', async () => {
+      const reqBody = { email: 'email', password: 'password' };
+
+      mock.onPost(`${AUTH_URL}auth`).reply(
+        200,
+        JSON.stringify({
+          data: {
+            user: {
+              name: 'username',
+            },
+            token: 'jwt.token.role',
+          },
+        })
+      );
+
+      await authService.authenticate(reqBody.email, reqBody.password);
+      expect(mock.history.post.length).toEqual(1);
+      expect(mock.history.post[0].data).toEqual(JSON.stringify(reqBody));
+      expect(mock.history.post[0].url).toEqual(`${AUTH_URL}auth`);
     });
+  });
 
-    describe('log in', () => {
-        it('makes a post to the AUTH_URL url', async () => {
-            const reqBody = { email: 'email', password: 'password' };
+  describe('register', () => {
+    it('makes a post to the AUTH_URL/user url', async () => {
+      const reqBody = { email: 'email', password: 'password' };
 
-            mock.onPost(`${AUTH_URL}auth`).reply(
-                200,
-                JSON.stringify({
-                    data: {
-                        user: {
-                            name: 'username'
-                        },
-                        token: 'jwt.token.role'
-                    }
-                })
-            );
+      mock.onPost(`${AUTH_URL}user`).reply(
+        200,
+        JSON.stringify({
+          data: {
+            user: {
+              name: 'username',
+            },
+            token: 'jwt.token.role',
+          },
+        })
+      );
 
-            await authService.authenticate(reqBody.email, reqBody.password);
-            expect(mock.history.post.length).toEqual(1);
-            expect(mock.history.post[0].data).toEqual(JSON.stringify(reqBody));
-            expect(mock.history.post[0].url).toEqual(`${AUTH_URL}auth`);
-        });
+      await authService.register(reqBody);
+      expect(mock.history.post.length).toEqual(1);
+      expect(mock.history.post[0].data).toEqual(JSON.stringify(reqBody));
+      expect(mock.history.post[0].url).toEqual(`${AUTH_URL}user`);
     });
+  });
 });
--- src/client/services/FormService/formService.test.js
+++ src/client/services/FormService/formService.test.js
@@ -3,21 +3,21 @@ import axios from 'axios';
 import { formService, FORM_URL } from './formService';
 
 describe('formService', () => {
-    const mock = new MockAdapter(axios);
+  const mock = new MockAdapter(axios);
 
-    beforeEach(() => {
-        mock.reset();
+  beforeEach(() => {
+    mock.reset();
+  });
+
+  describe('postForm', () => {
+    it('makes a post to the API_FORM url', async () => {
+      mock.onPost(FORM_URL).reply(200, { test: 'response' });
+
+      await formService.postForm({ test: 'form' });
+
+      expect(mock.history.post.length).toEqual(1);
+      expect(mock.history.post[0].data).toEqual(JSON.stringify({ test: 'form' }));
+      expect(mock.history.post[0].url).toEqual(FORM_URL);
     });
-
-    describe('submit form', () => {
-        it('makes a post to the API_FORM url', async () => {
-            mock.onPost(FORM_URL).reply(200, { test: 'response' });
-
-            await formService.postForm({ test: 'form' });
-
-            expect(mock.history.post.length).toEqual(1);
-            expect(mock.history.post[0].data).toEqual(JSON.stringify({ test: 'form' }));
-            expect(mock.history.post[0].url).toEqual(FORM_URL);
-        });
-    });
+  });
 });
