f2032d3d0a50b8c955b59bff8831a5ea3fa8784f
Matt Watts <matthewwatts@northwesternmutual.com>
2019-10-22 14:48:03 -0500

    Add formDefReducer tests.

--- src/client/redux/reducers/tests/formDefReducer.test.js
+++ src/client/redux/reducers/tests/formDefReducer.test.js
@@ -1,5 +1,105 @@
+import formDefReducer from '../formDefReducer';
+import * as actions from '../../actions/formDefActions';
+
 describe('formDefReducer', () => {
-    it('should work', () => {
-        // TODO
+  describe('POST', () => {
+    it('should return initial state', () => {
+      expect(formDefReducer(undefined, {})).toEqual({});
     });
+
+    it('should handle POST_FORM_DEF', () => {
+      expect(formDefReducer({}, { type: actions.POST_FORM_DEF })).toEqual({ status: actions.POST_FORM_DEF });
+    });
+
+    it('should handle POST_FORM_DEF_SUCCESS', () => {
+      expect(formDefReducer({}, { type: actions.POST_FORM_DEF_SUCCESS })).toEqual({
+        status: actions.POST_FORM_DEF_SUCCESS,
+      });
+    });
+
+    it('should handle POST_FORM_DEF_ERROR', () => {
+      expect(formDefReducer({}, { type: actions.POST_FORM_DEF_ERROR, payload: 'error' })).toEqual({
+        status: actions.POST_FORM_DEF_ERROR,
+        errorMessage: 'error',
+      });
+    });
+  });
+
+  describe('DELETE', () => {
+    it('should return initial state', () => {
+      expect(formDefReducer(undefined, {})).toEqual({});
+    });
+
+    it('should handle DELETE_FORM_DEF', () => {
+      expect(formDefReducer({}, { type: actions.DELETE_FORM_DEF })).toEqual({ status: actions.DELETE_FORM_DEF });
+    });
+
+    it('should handle DELETE_FORM_DEF_SUCCESS', () => {
+      expect(formDefReducer({}, { type: actions.DELETE_FORM_DEF_SUCCESS })).toEqual({
+        status: actions.DELETE_FORM_DEF_SUCCESS,
+      });
+    });
+
+    it('should handle DELETE_FORM_DEF_ERROR', () => {
+      expect(formDefReducer({}, { type: actions.DELETE_FORM_DEF_ERROR, payload: 'error' })).toEqual({
+        status: actions.DELETE_FORM_DEF_ERROR,
+        errorMessage: 'error',
+      });
+    });
+  });
+
+  describe('GET', () => {
+    it('should return initial state', () => {
+      expect(formDefReducer(undefined, {})).toEqual({});
+    });
+
+    it('should handle GET_FORM_DEF', () => {
+      expect(formDefReducer({}, { type: actions.GET_FORM_DEF })).toEqual({ status: actions.GET_FORM_DEF });
+    });
+
+    it('should handle GET_FORM_DEF_SUCCESS', () => {
+      expect(formDefReducer({}, { type: actions.GET_FORM_DEF_SUCCESS })).toEqual({
+        status: actions.GET_FORM_DEF_SUCCESS,
+      });
+    });
+
+    it('should handle GET_FORM_DEF_ERROR', () => {
+      expect(formDefReducer({}, { type: actions.GET_FORM_DEF_ERROR, payload: 'error' })).toEqual({
+        status: actions.GET_FORM_DEF_ERROR,
+        errorMessage: 'error',
+      });
+    });
+  });
+
+  describe('GET_ALL', () => {
+    it('should return initial state', () => {
+      expect(formDefReducer(undefined, {})).toEqual({});
+    });
+
+    it('should handle GET_ALL_FORM_DEFS', () => {
+      expect(formDefReducer({}, { type: actions.GET_ALL_FORM_DEFS })).toEqual({ status: actions.GET_ALL_FORM_DEFS });
+    });
+
+    it('should handle GET_ALL_FORM_DEFS_SUCCESS', () => {
+      expect(formDefReducer({}, { type: actions.GET_ALL_FORM_DEFS_SUCCESS })).toEqual({
+        status: actions.GET_ALL_FORM_DEFS_SUCCESS,
+      });
+    });
+
+    it('should handle GET_ALL_FORM_DEFS_ERROR', () => {
+      expect(formDefReducer({}, { type: actions.GET_ALL_FORM_DEFS_ERROR, payload: 'error' })).toEqual({
+        status: actions.GET_ALL_FORM_DEFS_ERROR,
+        errorMessage: 'error',
+      });
+    });
+  });
+
+  describe('CLEAR_ERROR', () => {
+    it('should clear error', () => {
+      expect(formDefReducer({}, { type: actions.FORM_DEF_CLEAR_ERROR })).toEqual({
+        status: actions.FORM_DEF_CLEAR_ERROR,
+        errorMessage: null,
+      });
+    });
+  });
 });
--- src/client/redux/reducers/tests/formReducer.test.js
+++ src/client/redux/reducers/tests/formReducer.test.js
@@ -2,22 +2,22 @@ import formReducer from '../formReducer';
 import * as actions from '../../actions/formActions';
 
 describe('formReducer', () => {
-    it('should return the initial state', () => {
-        expect(formReducer(undefined, {})).toEqual({});
-    });
+  it('should return initial state', () => {
+    expect(formReducer(undefined, {})).toEqual({});
+  });
 
-    it('should set isFormSubmitting to true and clear error', () => {
-        expect(formReducer({}, { type: actions.POST_FORM })).toEqual({ status: actions.POST_FORM });
-    });
+  it('should handle POST_FORM', () => {
+    expect(formReducer({}, { type: actions.POST_FORM })).toEqual({ status: actions.POST_FORM });
+  });
 
-    it('should set isFormSubmitting to false and clear error', () => {
-        expect(formReducer({}, { type: actions.POST_FORM_SUCCESS })).toEqual({ status: actions.POST_FORM_SUCCESS });
-    });
+  it('should handle POST_FORM_SUCCESS', () => {
+    expect(formReducer({}, { type: actions.POST_FORM_SUCCESS })).toEqual({ status: actions.POST_FORM_SUCCESS });
+  });
 
-    it('should return an error', () => {
-        expect(formReducer({}, { type: actions.POST_FORM_ERROR, payload: 'error' })).toEqual({
-            status: actions.POST_FORM_ERROR,
-            errorMessage: 'error'
-        });
+  it('should handle POST_FORM_ERROR', () => {
+    expect(formReducer({}, { type: actions.POST_FORM_ERROR, payload: 'error' })).toEqual({
+      status: actions.POST_FORM_ERROR,
+      errorMessage: 'error',
     });
+  });
 });
