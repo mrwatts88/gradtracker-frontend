40e56889bf63f3ced3809576db99e7223b3bed64
Matt Watts <matthewwatts@northwesternmutual.com>
2019-10-26 14:00:06 -0500

    Add ability to edit existing form submissions

--- src/client/components/FormSubmissionsAccordion/index.js
+++ src/client/components/FormSubmissionsAccordion/index.js
@@ -2,6 +2,7 @@ import { Collapse, Icon } from 'antd';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getAllFormSubsByUser } from '../../redux/actions/formActions';
+import { SubmissionForm } from './submissionForm';
 
 const { Panel } = Collapse;
 
@@ -9,17 +10,54 @@ function callback(key) {
   console.log(key);
 }
 
-const renderEdit = () => (
-  <Icon
-    type='edit'
-    onClick={event => {
-      event.stopPropagation();
-      console.log(event.target);
-    }}
-  />
-);
-
 export class FormSubmissionsAccordion extends Component {
+  state = {
+    currentlyEditing: [],
+  };
+
+  renderEdit = subId => {
+    return this.state.currentlyEditing.includes(subId) ? (
+      <React.Fragment>
+        <Icon
+          type="save"
+          style={{ color: 'green' }}
+          onClick={event => {
+            event.stopPropagation();
+            const currentlyEditing = [...this.state.currentlyEditing].filter(id => id !== subId);
+            this.setState({ currentlyEditing });
+            console.log(subId);
+          }}
+        />
+        &nbsp;
+        <Icon
+          style={{ color: 'red' }}
+          type="stop"
+          onClick={event => {
+            event.stopPropagation();
+            const currentlyEditing = [...this.state.currentlyEditing].filter(id => id !== subId);
+            this.setState({ currentlyEditing });
+            console.log(subId);
+          }}
+        />
+      </React.Fragment>
+    ) : (
+      <Icon
+        type="edit"
+        onClick={event => {
+          event.stopPropagation();
+          const currentlyEditing = [...this.state.currentlyEditing];
+          currentlyEditing.push(subId);
+          this.setState({ currentlyEditing });
+          console.log(subId);
+        }}
+      />
+    );
+  };
+
+  onInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   componentDidMount() {
     this.props.getAllFormSubsByUser(1);
   }
@@ -29,14 +67,15 @@ export class FormSubmissionsAccordion extends Component {
       <div>
         <Collapse onChange={callback}>
           {(this.props.submissions || []).map(s => {
-            return <Panel
-              header={`${s.name} - ${s.submissionDate} ${s.approved ? '' : '(pending)'}`}
-              key={s.id}
-              extra={!s.approved && renderEdit()}>
-              <div>{s.fields.sort((a, b) => (a.fieldIndex - b.fieldIndex)).map(field => {
-                return <div key={field.id}>{field.name}:{field.data}</div>;
-              })}</div>
-            </Panel>;
+            return (
+              <Panel
+                header={`${s.name} - ${s.submissionDate} ${s.approved ? '' : '(pending)'}`}
+                key={s.id}
+                extra={!s.approved && this.renderEdit(s.id)}
+              >
+                <SubmissionForm submission={s} currentlyEditing={this.state.currentlyEditing.includes(s.id)} />
+              </Panel>
+            );
           })}
         </Collapse>
         <br />
@@ -49,4 +88,7 @@ const mapStateToProps = ({ formReducer }) => ({
   submissions: formReducer.submissions,
 });
 
-export default connect(mapStateToProps, { getAllFormSubsByUser })(FormSubmissionsAccordion);
+export default connect(
+  mapStateToProps,
+  { getAllFormSubsByUser }
+)(FormSubmissionsAccordion);
---
+++ src/client/components/FormSubmissionsAccordion/submissionForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Form, Input } from 'antd';
+
+class S extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, form) => {
+      this.validateFields(err, form);
+    });
+  };
+
+  validateFields = (err, form) => {
+    if (!err) {
+      this.props.postForm(form);
+    }
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit} className="submission-form">
+          {this.props.submission.fields
+            .sort((a, b) => a.fieldIndex - b.fieldIndex)
+            .map(field => {
+              return this.props.currentlyEditing ? (
+                <div key={field.id}>
+                  <div style={{ fontWeight: 'bold' }}>{field.name}</div>
+                  <Form.Item key={field.id}>
+                    {getFieldDecorator(String(field.id), {
+                      initialValue: field.data,
+                      rules: [{ required: true, message: `${field.name} required.` }],
+                    })(<Input />)}
+                  </Form.Item>
+                  <br />
+                </div>
+              ) : (
+                <div key={field.id}>
+                  <div style={{ fontWeight: 'bold' }}>{field.name}</div>
+                  <div>{field.data}</div>
+                  <br />
+                </div>
+              );
+            })}
+        </Form>
+      </div>
+    );
+  }
+}
+
+export const SubmissionForm = Form.create({ name: 'submission_form' })(S);
